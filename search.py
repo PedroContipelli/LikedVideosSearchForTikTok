42ec27f48e957e93a1826a4c680c98b52193e7ee
Pedro Contipelli <Pcontipelli10@hotmail.com>
2020-09-27 11:53:24 -0400

    Add files via upload

---
+++ search.py
@@ -0,0 +1,23 @@
+from TikTokApi import TikTokApi
+api = TikTokApi()
+
+username = input('Enter your username: ')
+print('Searching in @' + username + '\'s liked videos...')
+
+num_videos = 1000
+
+liked_vids = api.userLikedbyUsername(username, count=num_videos)
+
+search_query = input('Search: ').lower()
+
+for tiktok in liked_vids:
+	search_values = [tiktok['desc'], tiktok['author']['uniqueId'], tiktok['author']['nickname'], tiktok['music']['title'], tiktok['music']['authorName']]
+	search_values = [str.lower() for str in search_values]
+
+	for search_val in search_values:
+		if search_query in search_val:
+			URL = 'https://www.tiktok.com/@' + tiktok['author']['uniqueId'] + '/video/' + tiktok['id']
+			print(URL)
+			break
+
+# End
